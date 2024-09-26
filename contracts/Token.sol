22f5d0a6779f4f73dfa94f298c172347351cbac6
Vadim <podoprigoravadim@rambler.ru>
2024-09-26 12:46:18 +0500

    More fixes

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    uint256 public TOTAL_SUPPLY_LIMIT;
+    uint256 public immutable TOTAL_SUPPLY_LIMIT;
     
     constructor(address owner, address[] memory initialAddresses, uint256 totalSupplylim) 
                 ERC20("MyToken", "TKN") 
