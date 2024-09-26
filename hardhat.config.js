53d21993b8947276cd4b59227b697b665dfb06ed
Vadim <podoprigoravadim@rambler.ru>
2024-09-26 12:19:48 +0500

    Added .env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/uo-JYAa7rMd_6mEZ3mwLEEk_exDhwLuJ", 
-      accounts: ["0x28085a1b0374c732b8b086b1862ce1495998e36977eeb6313075846e6620589c"] 
+      url: process.env.ALCHEMY_API_URL, 
+      accounts: [process.env.PRIVATE_KEY] 
     }
   }
 };
